ca1afb1896aaa6d1f3c9fad03b08d515c3334c95
petesteele98 <154248704+petesteele98@users.noreply.github.com>
2024-08-01 11:32:08 +0100

    change dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,14 +3,8 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Update package lists for Red Hat-based systems
+#
 sudo yum update -y
-
-# Install Node.js and npm
 sudo yum install -y nodejs npm
-
-# Remove existing node_modules and package-lock.json if they exist
 rm -rf node_modules package-lock.json
-
-# Install dependencies
 npm install
