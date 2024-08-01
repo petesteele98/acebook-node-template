b7bab54fbabf3701349669caf8014e7f67a980fe
petesteele98 <154248704+petesteele98@users.noreply.github.com>
2024-08-01 12:30:22 +0100

    remove npm install from install depenedncices

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
+
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Check if nodejs and npm are installed
-if ! command -v node &> /dev/null; then
-    sudo yum install -y nodejs npm
-fi
+#
+sudo yum update -y
+sudo yum install -y nodejs npm
+rm -rf node_modules package-lock.json
 
-# Check if node_modules exists and is not empty
-if [ -d node_modules ] && [ "$(ls -A node_modules)" ]; then
-    echo "node_modules directory exists and is not empty. Skipping npm install."
-else
-    rm -rf node_modules package-lock.json
-    npm install
-fi
